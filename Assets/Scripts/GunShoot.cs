0121704c453bd7a27017988343cc800289d91e4e
EUSUNGKIM <jimmy0818@naver.com>
2024-10-22 14:57:43 +0900

    [Update] 총소리 총알자국 구현
    총소리와 총알자국을 구현함

--- Assets/Scripts/GunShoot.cs
+++ Assets/Scripts/GunShoot.cs
@@ -5,32 +5,36 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class GunShoot : MonoBehaviour
 {
-    public GameObject bulletPrefab;
+    public GameObject bulletjakugprepap;
     public Transform bulletSpawnPoint;
-    public float bulletForce = 20f;
+    public float bulletRange = 100f;
+    public AudioSource gunAudioSound;
 
     public XRGrabInteractable grabInteractable;
 
     public void Start()
     {
-        // XRGrabInteractable 컴포넌트 가져오기
         grabInteractable = GetComponent<XRGrabInteractable>();
-
-        // Activated 이벤트 등록 (트리거 버튼을 누를 때 발생)
         grabInteractable.activated.AddListener(Shoot);
     }
 
     public void Shoot(ActivateEventArgs args)
     {
-        // 총알 생성
-        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
-
-        // 총알에 힘을 가해 발사
-        rb.AddForce(bulletSpawnPoint.forward * bulletForce, ForceMode.Impulse);
+        RaycastHit hit;
+        if (Physics.Raycast(bulletSpawnPoint.position, bulletSpawnPoint.forward, out hit, bulletRange))
+        {
+            if (bulletjakugprepap != null)
+            {
+                Instantiate(bulletjakugprepap, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+        if (gunAudioSound != null)
+        {
+            gunAudioSound.Play();
+        }
     }
 
-    public void OnDestroy()
+    void OnDestroy()
     {
         grabInteractable.activated.RemoveListener(Shoot);
     }
